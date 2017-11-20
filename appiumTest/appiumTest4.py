fc2b2fa44e536bbf73d1a360f406914c087e1e90
unityka <2198645229@qq.com>
2017-11-20 15:21:56 +0800

    提交APPIUM最后一次

---
+++ appiumTest/appiumTest4.py
@@ -0,0 +1,14 @@
+#coding:utf-8
+from appium import webdriver
+import time,traceback
+
+desired_caps={}
+desired_caps['platformName']='Andriod'
+desired_caps['platformVersion']='7.0'
+desired_caps['deviceName']='test'
+desired_caps['app']
+desired_caps['appPackage']
+desired_caps['appActivity']
+desired_caps['noSet']
+desired_caps['unicodeKeyboard']
+desired_caps['commandTimeout']
